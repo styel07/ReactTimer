9adf78c28e1e4bdaed04f116fc13d02825398747
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-06-26 00:51:32 -0700

    initial commit, add navigation and styles

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+// create out app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+// static specifies the folder name that exposes the main foler
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+})
+
+app.use(express.static('public'));
+
+app.listen(PORT, function() {
+  console.log('server is up on port '+PORT);
+});
